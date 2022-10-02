c63ebf6b28b60ad1e6e4671b273c9dd823c4eb28
DESKTOP-063BMPJ\Vinny <vvauban@gmail.com>
2022-10-02 19:44:34 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
-import logo from './logo.svg';
+import { useState, useEffect } from "react";
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [activity, setActivity] = useState([]);
+
+  // https://www.boredapi.com/api/activity
+
+
+  function fetchActivityHandler() {
+    fetch('https://www.boredapi.com/api/activity').then(response =>
+      response.json()).then(data => {
+        console.log(data);
+        setActivity(data);
+      }
+      );
+  }
+
+  useEffect(() => {
+    fetchActivityHandler();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p><h3><b>Activity</b>: {activity.activity}</h3><br /></p>
+        <p><b>🏷️ Type</b>: {activity.type}<br /></p>
+        <p><b>👯 Participants</b>: {activity.participants}<br /></p>
+        <p><b>💰 Price</b>: {activity.price}<br /></p>
+        <p><b>🌐 Link</b>: {activity.link}<br /></p>
+        <p><b>🔑 Key</b>: {activity.key}<br /></p>
+        <p><b>♿ Accessibility</b>: {activity.accessibility}<br /></p>
       </header>
     </div>
   );
